vnect-like net updated to correct backprop

src/main.py
@@ -6,7 +6,7 @@ import numpy as np
 import argparse
 import torch
 
-from nn import MyNet
+from nn import MyNet, VNect
 from solver import Solver
 from dataHandler import TrainDataHandler, TestDataHandler
 from utils import Plotter
@@ -31,6 +31,8 @@ parser.add_argument('--plot', action='store_true', default=False,
                     help='enables plot train and validation histories')
 parser.add_argument('--visdom', action='store_true', default=False,
                     help='enables VISDOM')
+parser.add_argument('--vnect', action='store_true', default=False,
+                    help='uses VNect-like network')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA')
 parser.add_argument('--seed', type=int, default=1, metavar='N',
@@ -63,7 +65,7 @@ print("Test size: %i" % len(test_data))
 print("Data dimensions:", train_data[0][0].size())
 
 ## LOAD MODELS & SOLVER
-model = MyNet()
+model = MyNet() if not args.vnect else VNect()
 checkpoint = {}
 if args.model:
   checkpoint = torch.load(args.model)

src/nn.py
@@ -118,6 +118,9 @@ class VNect(MyNN):
     self.conv1 = nn.Conv2d(240, 128, 3, stride=1, padding=1)
     self.conv2 = nn.Conv2d(128, 112, 1)
 
+    self.fc1 = nn.Linear(716800, 252)
+    self.fc2 = nn.Linear(252, 84)
+
   def forward(self, x):
     out = self.resnet(x) # (1024,w/16,h/16)
 
@@ -132,10 +135,15 @@ class VNect(MyNN):
     out3 = torch.sqrt(out3) # (28,w/8,h/8)
     out = torch.cat((out,out3),1) # (240,w/8,h/8)
 
-    out = F.relu(self.conv1(out))
-    out = self.conv2(out)
+    out = F.relu(self.conv1(out)) # (128,w/8,h/8)
+    out = self.conv2(out) # (112,w/8,h/8)
+    out = out.view(out.size(0),4,out.size(-1),out.size(-2),28)
 
-    return out
+    out_mod = out.view(out.size(0), -1)
+    out_mod = F.relu(self.fc1(out_mod))
+    out_mod = self.fc2(out_mod)
+
+    return out_mod
 
 class Residual(nn.Module):
   def __init__(self, numIn, numOut, residual=True):
@@ -161,16 +169,3 @@ class Residual(nn.Module):
     if (self.numIn != self.numOut & self.residual):
       residual = self.conv4(x)
     return out + residual
-
-# if __name__ == '__main__':
-#   net = VNect()
-#   net.eval()
-#   import numpy as np
-#   import torch
-#   from torch.autograd import Variable
-#   print(net.forward(Variable(torch.from_numpy(np.random.rand(1,3,320,320)).float())).shape)
-
-#   import pickle
-#   model_weights = pickle.load(open('../../vnect.pkl', 'rb'), encoding='latin1')
-#   print(model_weights.keys())
-#   print(model_weights['res5c_branch2b/weights'].shape)